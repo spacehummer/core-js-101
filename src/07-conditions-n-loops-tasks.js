1e6795bb126997b980209d0001fec3ef582f0822
Yuriy Lavrushin <yuriy.lavroushin@gmail.com>
2023-06-29 15:09:22 +0400

    feat: module `07-conditions-n-loops-tasks.js`: add few new solutions, finish module.

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -408,9 +408,14 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
+function getDigitalRoot(num) {
   // fix error in JSDoc (n -> num)
-  throw new Error('Not implemented');
+  // Using recursion.
+  const currentSum = num.toString(10).split('')
+    .reduce((acc, cur) => acc + Number(cur), 0);
+  // eslint-disable-next-line no-console
+  // console.log(currentSum);
+  return currentSum > 9 ? getDigitalRoot(currentSum) : currentSum;
 }
 
 
@@ -435,8 +440,197 @@ function getDigitalRoot(/* num */) {
  *   '{)' = false
  *   '{[(<{[]}>)]}' = true
  */
-function isBracketsBalanced(/* str */) {
-  throw new Error('Not implemented');
+function isBracketsBalanced(str) {
+  // eslint-disable-next-line no-console
+  // console.log(str);
+
+  function check(strLocal, bracketsConfig = [
+    ['(', ')'],
+    ['[', ']'],
+    ['{', '}'],
+    ['|', '|'],
+    ['<', '>'],
+  ]) {
+    /*
+     * Использовался алгоритм с двумя стеками. Имелись ложные срабатывания `|()|(||)||`.
+     * */
+
+    /*
+     * Вторая версия: алгоритм с одним стеком для обоих типов скобок. Сразу работаю с исходным
+     * конфигом в виде двумерного массива.
+     * */
+
+    /**
+     * Main brackets check function.
+     */
+    function checkBrackets(checkedStr, bracketsConfigLocal) {
+      // Debug info: objects to watch:
+      // str, charIndex, currentSymbol, similarBracketsStack,
+      // openBracketsStack, similarBracketsStatus, regularBracketsStatus
+
+      // Вывести в лог текущую строку со скобками:
+      // console.log('\nTesting string:', checkedStr);
+
+      // Вывести в лог текущий конфиг проверяемых скобок:
+      // console.log('Brackets config:', bracketsConfig);
+
+      // Массив, описывающий типы открывающих скобок
+      // const openBracketsTypesArr = ['(', '{', '[', '|'];
+
+      // Словарь (map), который задаёт пару скобок. Он из себя на уровне JS представляет
+      // объект.
+      // Ключи - закрывающие скобки.
+      // Значения - открывающие скобки.
+      // Используя эту структуру, будем проверять, какой тип скобки должен
+      // лежать на верхушке стека, чтобы его убрать.
+      // const bracketsPairsMap = {
+      //   ')': '(',
+      //   '}': '{',
+      //   ']': '[',
+      // };
+
+      // const similarBracketsPairMap = {
+      //   '|': '|',
+      // };
+
+      // Создаём стек для накопления открывающих скобок, которые встречаются в строке.
+      // Когда находим закрывающую - убираем последнюю открывающую из стека,
+      // удаляя верхний элемент.
+      const openBracketsStack = [];
+
+      // Создаём стек для скобок такого типа, при котором открывающаяся и закрывающая одним и тем
+      // же символом обозначается.
+      // let similarBracketsStack = [];
+
+      // Переменные для хранения результата анализа для каждого типа скобок.
+      // let regularBracketsStatus = true;
+      // let similarBracketsStatus = true;
+
+      // Переменная для хранения состояния того, отработало ли хотя бы одно условие на проверку
+      // классических парных скобок.
+      // Not used in current version.
+      // const regularBracketsStatementsStatus = false;
+
+      // Необходимо найти способ работать с исходным конфигом скобок.
+      // Если работаем, как со строкой. `.replace()` с регулярным выражением уберёт скобки,
+      // которые появятся из-за вложенности массивов.
+      // Not used in current version.
+      // const bracketsConfigStr = bracketsConfigLocal.join('').replace(/,/g, '');
+
+
+      // Функция для проверки типа скобки (является ли она открывающейся).
+      // Если работаем с исходным массивом.
+      function isBracketsIsOpen(bracketsConfigLocalLocal, bracketChecked) {
+        let isOpenStatus = false;
+
+        bracketsConfigLocalLocal.forEach((bracketPair) => {
+          if (bracketPair[0] === bracketChecked) {
+            isOpenStatus = true;
+          }
+        });
+
+        return isOpenStatus;
+      }
+
+      // Функция для получения идентификатора типа скобки исходя из данной конфигурации скобок.
+      // Это будет номер вложенного массива с парой скобок.
+      // Функция применяется, если работаем с исходным массивом.
+      function getBracketsPairTypeNumber(bracketsConfigLocalLocal, bracketChecked) {
+        let bracketPairIndexOutput = null;
+
+        bracketsConfigLocalLocal.forEach((bracketPair, bracketPairIndex) => {
+          if (bracketPair.includes(bracketChecked)) {
+            bracketPairIndexOutput = bracketPairIndex;
+          }
+        });
+
+        return bracketPairIndexOutput;
+      }
+
+
+      // Main loop - for iterate through source string
+      for (let charIndex = 0; charIndex < checkedStr.length; charIndex += 1) {
+        // Temporal const (update every iteration) - current symbol of source string.
+        const currentSymbol = checkedStr[charIndex];
+
+        // Работаем с конфигом скобок, как с массивом двумерным, строка не нужна.
+        // let bracketsIndex = bracketsConfigStr.indexOf(currentSymbol)
+
+        // Get brackets pair type from config: it is number of inner Array in outer Array.
+        // 0 < bracketsPairTypeNumber < bracketsConfig.length - 1
+        const bracketsPairTypeNumber = getBracketsPairTypeNumber(
+          bracketsConfigLocal,
+          currentSymbol,
+        );
+
+        // Get brackets type of pair (opening or closing) by it position in inner config Array.
+        // 0 - opening;
+        // 1 - closing.
+        const bracketsInPairIndex = bracketsConfigLocal[bracketsPairTypeNumber]
+          .indexOf(currentSymbol);
+
+        // Класть в стек и обрабатывать объект с информаций о скобке?..
+        // Not used in current version.
+        // const bracketInfo = {
+        //   pairTypeNumber: bracketsPairTypeNumber,
+        //   bracketsInPairIndex,
+        // };
+
+        // Текущая скобка открывающаяся?
+        if (isBracketsIsOpen(bracketsConfigLocal, currentSymbol)) {
+          // Если текущая скобка (открывающаяся), обозначена таким же символом, как закрывающаяся,
+          // и текущий тип скобок (тип текущей скобки) лежит в стеке сверху:
+          // => извлечь из стека верхний элемент.
+          if (currentSymbol === bracketsConfigLocal[bracketsPairTypeNumber][1]
+            && openBracketsStack[openBracketsStack.length
+            - 1] === bracketsConfigLocal[bracketsPairTypeNumber][bracketsInPairIndex]) {
+            openBracketsStack.pop();
+            // иначе: если текущая скобка (открывающаяся), обозначена таким же символом, как
+            // закрывающаяся, и текущий тип скобок не соответствует типу скобки, которая лежит в
+            // стеке сверху: => добавить текущую скобку в стек.
+          } else if (
+            currentSymbol === bracketsConfigLocal[bracketsPairTypeNumber][1]
+            && openBracketsStack[openBracketsStack.length
+            - 1] !== bracketsConfigLocal[bracketsPairTypeNumber][bracketsInPairIndex]) {
+            openBracketsStack
+              .push(bracketsConfigLocal[bracketsPairTypeNumber][bracketsInPairIndex]);
+          } else {
+            // иначе (это если скобка разными символами слева и справа обозначается):
+            // => добавить текущую скобку в стек.
+            openBracketsStack
+              .push(bracketsConfigLocal[bracketsPairTypeNumber][bracketsInPairIndex]);
+          }
+        } else {
+          // иначе (если текущая скобка закрывающаяся, причём закрывающаяся кодируемая разными
+          // символами слева и справа):
+
+          // Если текущая скобка, которая должна быть закрывающейся, не является таковой
+          // для скобки, лежащей вверху стека:
+          // => остановить выполнение функции и вернуть `false`.
+          if (openBracketsStack[openBracketsStack.length
+          - 1] !== bracketsConfigLocal[bracketsPairTypeNumber][bracketsInPairIndex - 1]) {
+            return false;
+          }
+
+          // В любом случае извлеки верхний элемент стека:
+          openBracketsStack.pop();
+        }
+      }
+
+      // Если проверки в цикле не прервали выполнение функции, но мы дошли до конца строки,
+      // проверяем, пустой ли стек.
+      // Если стек пустой - то вернётся `true` - все скобки закрываются и отбалансированы.
+      // Если строка закончилась, а стек не пустой - то вернётся `false` - какие-то скобки не
+      // закрыты.
+      return openBracketsStack.length === 0;
+    }
+
+    return checkBrackets(strLocal, bracketsConfig);
+  }
+
+  return check(str);
+
+  // throw new Error('Not implemented');
 }
 
 
@@ -477,8 +671,43 @@ function toNaryString(num, n) {
  *   ['/web/assets/style.css', '/.bin/mocha',  '/read.me'] => '/'
  *   ['/web/favicon.ico', '/web-scripts/dump', '/verbalizer/logs'] => '/'
  */
-function getCommonDirectoryPath(/* pathes */) {
-  throw new Error('Not implemented');
+function getCommonDirectoryPath(pathes) {
+  // eslint-disable-next-line no-console
+  // console.log(pathes);
+  if (pathes.filter((path) => path[0] === '/').length < pathes.length) {
+    return '';
+  }
+
+  const similarComponents = [];
+  const pathesExploded = pathes.map((path) => path.replace(new RegExp(/\//, ''), '').split('/'));
+  // eslint-disable-next-line no-console
+  console.log(pathesExploded);
+  const pathLengths = pathesExploded.map((path) => path.length);
+  const maxLength = Math.max(...pathLengths);
+  // eslint-disable-next-line prefer-const
+  let isSimilar = true;
+
+  for (let i = 0; (i < maxLength) && (isSimilar); i += 1) {
+    for (let j = 0; j < (pathesExploded.length) && (isSimilar); j += 1) {
+      if (j === 0) {
+        similarComponents.push(pathesExploded[j][i]);
+        // eslint-disable-next-line no-console
+        console.log(similarComponents);
+      }
+      if (similarComponents[i] !== pathesExploded[j][i]) {
+        isSimilar = false;
+        similarComponents.pop();
+      }
+    }
+  }
+
+  const similarComponentsRawStr = similarComponents.join('/');
+
+  // eslint-disable-next-line no-console
+  console.log(similarComponentsRawStr === '' ? '/' : `/${similarComponentsRawStr}/`);
+
+  // return '/web/images/';
+  return similarComponentsRawStr === '' ? '/' : `/${similarComponentsRawStr}/`;
 }
 
 
